28e0f7705ac9211f74f686c82084b01d965bc4c2
unknown <jgn1234567@gmail.com>
2020-11-30 11:12:51 -0600

    Created new html template files

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -13,17 +13,16 @@ class MainController extends AbstractController
      * @Route("/", name="home")
      */
     public function index(){
-        return new Response( '<h1>Welcome to our first page!!!!!</h1>');
-        //return $this->render( view :'home/index.html.twig')
+        return $this->render("home/index.html.twig");
     }
 
     /**
      * @Route("/custom/{name?}", name="custom")
      */
     public function custom(Request $request){
-        dump($request);
+//        dump($request);
         $name = $request->get('name');
-        return new Response( '<h1>Welcome ' . $name . '</h1>');
+        return $this->render("home/custom.html.twig" , [ 'customName' => $name ]);
     }
 
 }
